e46e4dcb2d54b5e26771c3a5452045edf1992c28
NguyenKhacHung22102006 <hungskyok06@gmail.com>
2024-11-21 00:06:15 +0700

    Add files via upload

---
+++ DEV C++ baif 2 session2.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("tinh tong hai so nguyen\n");
+    int a,b;
+    printf("nhap a:\n");
+    scanf("%d",&a);
+    printf("nhap b:\n");
+    scanf("%d",&b);
+    int tong=a+b;
+    printf("tong cua a va b la: %d\n",tong);
+    printf("\n");
+    return 0;
+}
