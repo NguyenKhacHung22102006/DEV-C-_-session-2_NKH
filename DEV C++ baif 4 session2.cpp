e361c1e130cb8b9d7c91f1c24578318eceacedbc
NguyenKhacHung22102006 <hungskyok06@gmail.com>
2024-11-21 00:08:58 +0700

    Add files via upload

---
+++ DEV C++ baif 4 session2.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("tinh dien tich hinh vuong\n");
+    float canh;
+    scanf("%f",&canh);
+    float DienTich = canh*canh;
+    printf("dien tich hinh vuong la:%f\n",DienTich);
+    printf("\n");
+    return 0;
+}
